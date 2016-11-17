4eaf220298baec2cc33d74e84b455b6eefce9d50
datovard <datovard@unal.edu.co>
2016-11-17 09:15:08 -0500

    Added change of marker when defined on forms and fixed the folders structure

--- Maps.js
+++ Maps.js
@@ -10,27 +10,35 @@ app.controller('MapNavigator', [ '$scope', '$compile', '$http', function( $scope
 	$scope.tipos = {};
 	$scope.tipo = null;
 	$scope.contentString = "";
+	$scope.directionsDisplay = null;
+	$scope.directionsService = new google.maps.DirectionsService();
+	
+	$scope.first = null;
+	$scope.second = null;
 	
 	$scope.markerForm = function(){
-		//console.log($scope.markers);
-		//console.log($scope.tipos);
 		var name = $scope.actual;
 		$scope.tipos[name] = $scope.tipo;
-		//$scope.infowindows[name].close();
+		console.log($scope.markers[name]);
+		$scope.markers[name].setIcon('Icons/'+$scope.tipo+'.png');
 		$scope.infowindow.close();
 	};
-
-	$scope.clickTest = function() {
-        console.log($scope.markers);
-		$scope.infowindows[$scope.actual].close();
-    };
-
+	
+	$scope.borrarMarker = function(){
+		var name = $scope.actual;
+		$scope.markers[name].setMap(null);
+		delete $scope.markers[name];
+	};
 
 	$scope.initMap = function () {
+		$scope.directionsDisplay = new google.maps.DirectionsRenderer();
+		
 		$scope.map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: 4.637368, lng: -74.082643},
 			zoom: 15,
 		});
+		
+		$scope.directionsDisplay.setMap(map);
 		
 		var drawingManager = new google.maps.drawing.DrawingManager({
 			drawingMode: google.maps.drawing.OverlayType.CIRCLE,
@@ -41,9 +49,7 @@ app.controller('MapNavigator', [ '$scope', '$compile', '$http', function( $scope
 					google.maps.drawing.OverlayType.MARKER			
 				]
 			},
-			markerOptions: { 
-				draggable : true,
-			}
+			markerOptions: { icon: 'Icons/marker.png' }
 		});
 		
 		$scope.contentString = '<div id="content">'+
@@ -57,8 +63,8 @@ app.controller('MapNavigator', [ '$scope', '$compile', '$http', function( $scope
 										'<input type="radio" ng-model="tipo" value="tipoDC">Centro de distribución'+
 										'<input type="radio" ng-model="tipo" value="tipoCliente">Cliente<br />'+
 										'<button type="submit" ng-click="markerForm()">Guardar</button>'+
+										'<button type="submit" ng-click="borrarMarker()">Borrar este punto</button>'+
 									'</form>'+
-									//'<button class="button button-clear button-positive" ng-click="clickTest()">Click Me</button>'+
 								'</div>'+
 							'</div>';
 
@@ -68,37 +74,30 @@ app.controller('MapNavigator', [ '$scope', '$compile', '$http', function( $scope
 			content: compiledContent[0]
 		});
 		
-		google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {	  
+		google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
 		  if (event.type == google.maps.drawing.OverlayType.MARKER) {
+			var marker = event.overlay;
+			
+			event.overlay.addListener('dblclick', function(event) {
+				$scope.infowindow.close();
+				var name = JSON.stringify(marker.position);
+				alert('llega');
+			});
+			
 			event.overlay.addListener('click', function(event) {
-				var name = JSON.stringify(event.latLng);
+				var name = JSON.stringify(marker.position);
 				$scope.infowindow.close();
 				if( !( name in $scope.markers ) ){
-					$scope.markers[name] = event;
+					$scope.markers[name] = marker;
 					$scope.tipo = null;
 					$scope.actual = name;
-					var contentString = '<div id="content">'+
-											'<div id="siteNotice">'+
-											'</div>'+
-											'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-											'<div id="bodyContent">'+
-												'<form onsubmit="return false">'+
-													'<input type="radio" ng-model="tipo" value="tipoProveedor">Proveedor'+
-													'<input type="radio" ng-model="tipo" value="tipoPlanta">Planta<br />'+
-													'<input type="radio" ng-model="tipo" value="tipoDC">Centro de distribución'+
-													'<input type="radio" ng-model="tipo" value="tipoCliente">Cliente<br />'+
-													'<button type="submit" ng-click="markerForm()">Guardar</button>'+
-												'</form>'+
-												//'<button class="button button-clear button-positive" ng-click="clickTest()">Click Me</button>'+
-											'</div>'+
-										'</div>';
 
 					var compiledContent = $compile($scope.contentString)($scope);
 					
 					$scope.infowindows[name] = new google.maps.InfoWindow({
 						content: compiledContent[0]
 					});
-					//$scope.infowindows[name].open($scope.map, this);
+					
 					$scope.infowindow.open($scope.map, this);
 				}else{
 					$scope.actual = name;
@@ -111,11 +110,9 @@ app.controller('MapNavigator', [ '$scope', '$compile', '$http', function( $scope
 						content: compiledContent[0]
 					});
 					
-					//$scope.infowindows[name].open($scope.map, this);
 					$scope.infowindow.open($scope.map, this);
 				}
 			});
-			
 		  }
 		});
 		drawingManager.setMap($scope.map);
@@ -123,9 +120,3 @@ app.controller('MapNavigator', [ '$scope', '$compile', '$http', function( $scope
 	
 	$scope.initMap();
 }]);
-
-/*function markerForm( event ){
-	console.log(markers);
-	console.log(event.target.parentNode.childNodes[0].checked );
-	event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[2].click();
-}*/
