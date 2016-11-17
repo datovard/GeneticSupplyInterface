e6fdc14fbe7198f99675ff37836810c73f307c5a
datovard <datovard@unal.edu.co>
2016-11-17 09:13:28 -0500

    fixed the forms in the markers

--- Maps.js
+++ Maps.js
@@ -5,11 +5,19 @@ app.controller('MapNavigator', [ '$scope', '$compile', '$http', function( $scope
 	$scope.map = null;
 	$scope.markers = {};
 	$scope.infowindows = {};
+	$scope.infowindow = null;
 	$scope.actual = null;
+	$scope.tipos = {};
+	$scope.tipo = null;
+	$scope.contentString = "";
 	
 	$scope.markerForm = function(){
-		console.log($scope.markers);
-		$scope.infowindows[$scope.actual].close();
+		//console.log($scope.markers);
+		//console.log($scope.tipos);
+		var name = $scope.actual;
+		$scope.tipos[name] = $scope.tipo;
+		//$scope.infowindows[name].close();
+		$scope.infowindow.close();
 	};
 
 	$scope.clickTest = function() {
@@ -38,36 +46,73 @@ app.controller('MapNavigator', [ '$scope', '$compile', '$http', function( $scope
 			}
 		});
 		
+		$scope.contentString = '<div id="content">'+
+								'<div id="siteNotice">'+
+								'</div>'+
+								'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+								'<div id="bodyContent">'+
+									'<form onsubmit="return false">'+
+										'<input type="radio" ng-model="tipo" value="tipoProveedor">Proveedor'+
+										'<input type="radio" ng-model="tipo" value="tipoPlanta">Planta<br />'+
+										'<input type="radio" ng-model="tipo" value="tipoDC">Centro de distribución'+
+										'<input type="radio" ng-model="tipo" value="tipoCliente">Cliente<br />'+
+										'<button type="submit" ng-click="markerForm()">Guardar</button>'+
+									'</form>'+
+									//'<button class="button button-clear button-positive" ng-click="clickTest()">Click Me</button>'+
+								'</div>'+
+							'</div>';
+
+		var compiledContent = $compile($scope.contentString)($scope);
+		
+		$scope.infowindow = new google.maps.InfoWindow({
+			content: compiledContent[0]
+		});
+		
 		google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {	  
 		  if (event.type == google.maps.drawing.OverlayType.MARKER) {
 			event.overlay.addListener('click', function(event) {
-				if( !( event in $scope.markers ) ){
-					var name = "marker"+Object.keys($scope.markers).length;
-					$scope.markers[event] = name;
-					
+				var name = JSON.stringify(event.latLng);
+				$scope.infowindow.close();
+				if( !( name in $scope.markers ) ){
+					$scope.markers[name] = event;
+					$scope.tipo = null;
+					$scope.actual = name;
 					var contentString = '<div id="content">'+
 											'<div id="siteNotice">'+
 											'</div>'+
 											'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
 											'<div id="bodyContent">'+
 												'<form onsubmit="return false">'+
-													'<input type="radio" name="tipos" id="tipoProveedor" value="tipoProveedor">Proveedor'+
-													'<input type="radio" name="tipos" id="tipoPlanta" value="tipoPlanta">Planta<br />'+
-													'<input type="radio" name="tipos" id="tipoDC" value="tipoDC">Centro de distribución'+
-													'<input type="radio" name="tipos" id="tipoCliente" value="tipoCliente">Cliente<br />'+
+													'<input type="radio" ng-model="tipo" value="tipoProveedor">Proveedor'+
+													'<input type="radio" ng-model="tipo" value="tipoPlanta">Planta<br />'+
+													'<input type="radio" ng-model="tipo" value="tipoDC">Centro de distribución'+
+													'<input type="radio" ng-model="tipo" value="tipoCliente">Cliente<br />'+
 													'<button type="submit" ng-click="markerForm()">Guardar</button>'+
 												'</form>'+
 												//'<button class="button button-clear button-positive" ng-click="clickTest()">Click Me</button>'+
 											'</div>'+
 										'</div>';
 
-					var compiledContent = $compile(contentString)($scope);
+					var compiledContent = $compile($scope.contentString)($scope);
 					
 					$scope.infowindows[name] = new google.maps.InfoWindow({
 						content: compiledContent[0]
 					});
+					//$scope.infowindows[name].open($scope.map, this);
+					$scope.infowindow.open($scope.map, this);
+				}else{
 					$scope.actual = name;
-					$scope.infowindows[name].open($scope.map, this);
+					$scope.tipo = $scope.tipos[name];
+					console.log($scope.tipo);
+					
+					var compiledContent = $compile($scope.contentString)($scope);
+		
+					$scope.infowindow = new google.maps.InfoWindow({
+						content: compiledContent[0]
+					});
+					
+					//$scope.infowindows[name].open($scope.map, this);
+					$scope.infowindow.open($scope.map, this);
 				}
 			});
 			
